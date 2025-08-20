c43af49cee93ef793890dbcaa686fc1a89dec54a
Dennis0811 <dennis.bauer96@gmx.de>
2025-08-20 16:18:25 +0200

    scrollable family and friends

--- src/Components/LeftColumn.tsx
+++ src/Components/LeftColumn.tsx
@@ -94,43 +94,48 @@ const LeftColumn = () => {
             {movieNight.movieTime}
           </p>
         </LeftColumnSection>
-        <LeftColumnSection title="Family and Friends" className="overflow-auto">
-          {friends.map((friend) => (
-            <div
-              key={friend.name}
-              className="flex flex-row items-center justify-between text-lg font-semibold hover:bg-hover-bg hover:text-primary-text transition p-2 space-x-2 rounded"
-            >
-              <div className="flex items-center space-x-2 min-w-fit">
-                <img
-                  src={`${friend.name.toLowerCase()}-logo.png`}
-                  alt={`${friend.name} Logo`}
-                  className="h-10 w-10 rounded-full"
-                />
-                <p>{friend.name}</p>
-              </div>
-              <div className="flex flex-row flex-wrap items-center justify-end gap-2">
-                {friend.links &&
-                  friend.links.length > 0 &&
-                  friend.links.map((link) => {
-                    const [type, url] = Object.entries(link)[0];
-                    const Icon = iconMap[type as keyof typeof iconMap];
+        <LeftColumnSection
+          title="Family and Friends"
+          className="overflow-y-auto"
+        >
+          <div className="overflow-y-auto">
+            {friends.map((friend) => (
+              <div
+                key={friend.name}
+                className="flex flex-row items-center justify-between text-lg font-semibold hover:bg-hover-bg hover:text-primary-text transition p-2 rounded"
+              >
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={`${friend.name.toLowerCase()}-logo.png`}
+                    alt={`${friend.name} Logo`}
+                    className="h-10 w-10 rounded-full"
+                  />
+                  <p>{friend.name}</p>
+                </div>
+                <div className="flex flex-row flex-wrap items-center justify-end gap-2">
+                  {friend.links &&
+                    friend.links.length > 0 &&
+                    friend.links.map((link) => {
+                      const [type, url] = Object.entries(link)[0];
+                      const Icon = iconMap[type as keyof typeof iconMap];
 
-                    return (
-                      <a
-                        key={type + url}
-                        href={url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-accent transition"
-                        aria-label={type}
-                      >
-                        {Icon && <Icon className="w-5 h-5" />}
-                      </a>
-                    );
-                  })}
+                      return (
+                        <a
+                          key={type + url}
+                          href={url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="hover:text-accent transition"
+                          aria-label={type}
+                        >
+                          {Icon && <Icon className="w-5 h-5" />}
+                        </a>
+                      );
+                    })}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </LeftColumnSection>
       </LeftColumnSections>
     </>
